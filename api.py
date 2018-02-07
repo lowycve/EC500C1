352ef741e864caf4b1017d5112f79bcb8a3d16d6
lowycve <31743714+lowycve@users.noreply.github.com>
2018-02-06 19:05:27 -0500

    Add files via upload

---
+++ api.py
@@ -0,0 +1,116 @@
+import tweepy #https://github.com/tweepy/tweepy
+import json
+import wget
+import requests
+import io
+import os
+from os import listdir
+from google.cloud import vision
+from google.cloud.vision import types
+import sys
+#Twitter API credentials
+consumer_key = "X81uhAUxCUv5xFsqmmWY1Uytt"
+consumer_secret = "oWvsm4D4SDke4mCSZ2fbmDeLAXkiQvWCcR7RPxDSWC1qcvQH8t"
+access_key = "956288592686534656-rSUsibsc3wrljniaq9BjkveIJcfsTkQ"
+access_secret = "vj8XH6UGG9Cm38vwv4V51ot39bwsFq9D50W7QuM4knJdO"
+
+
+def get_all_tweets(screen_name):
+    
+    #Twitter only allows access to a users most recent 3240 tweets with this method
+    
+    #authorize twitter, initialize tweepy
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+    
+    #initialize a list to hold all the tweepy Tweets
+    alltweets = []    
+    
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=10)
+    if len(new_tweets) == 0:
+        print('The user did not tweet anything.')
+        sys.exit()
+    #save most recent tweets
+    alltweets.extend(new_tweets)
+    
+    #save the id of the oldest tweet less one
+    oldest = alltweets[-1].id - 1
+    #keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0:
+        
+        #all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name = screen_name,count=10,max_id=oldest)
+        
+        #save most recent tweets
+        alltweets.extend(new_tweets)
+        
+        #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+        if(len(alltweets) > 15):
+            print('more that 15 tweets downloaded.')
+            break
+        print ("There are %s tweets downloaded so far" % (len(alltweets)))
+
+    media_files = set()
+
+    for status in alltweets:
+        try:
+            media = status.extended_entities.get('media', [])
+        except:
+            media = status.entities.get('media', [])
+        if(len(media) > 0):
+            for i in range(len(media)):
+                media_files.add(media[i]['media_url'])
+    if len(media_files) == 0:
+        print('The user did not post any pictures.')
+        sys.exit()
+    for media_file in media_files:
+        wget.download(media_file)
+    
+
+# get_all_tweets("@lm19official")
+try:
+    # get_all_tweets("@8POrwwRMMS8gqkJ")
+    # get_all_tweets("@dsarew")
+    get_all_tweets("@Yuchen310")
+except tweepy.error.TweepError:
+    print('Sorry, the user is not exist')
+    sys.exit()
+
+GOOGLE_APPLICATION_CREDENTIALS = './service-account-file.json'
+client = vision.ImageAnnotatorClient()
+
+file = open('result.txt','w')
+m = 1
+OBJ = []
+for pic in listdir():
+    if pic.endswith('jpg') or pic.endswith('png'):
+        OBJ.append(pic)
+print(str(len(OBJ)) + ' Pictures Found.' + '\n')
+file.write(str(len(OBJ)) + ' Pictures Found.' + '\n')
+os.system('ffmpeg -framerate 1 -pattern_type glob -i "*.jpg"   -c:v libx264 -r 30 -pix_fmt yuv420p 1.mpg')
+os.system('ffmpeg -framerate 1 -pattern_type glob -i "*.png"   -c:v libx264 -r 30 -pix_fmt yuv420p 2.mpg')
+os.system('cat 1.mpg 2.mpg | ffmpeg -f mpeg -i - -qscale 0 -vcodec mpeg4 output.mp4')
+for i in OBJ:
+    file_name = os.path.join(os.path.dirname(__file__),i)
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+    image = types.Image(content=content)
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    # writing Picutre names and  IDs to txt.file
+    print('Picture Name: ' + i + ', ID: ' + str(m))
+    print(str(len(labels)) + ' objects detected.')
+    print('Labels:')
+    file.write('Picture Name: ' + i + ', ID: ' + str(m) + '\n')
+    file.write(str(len(labels)) + ' objects detected.' + '\n')
+    file.write('Labels:' + '\n')
+    m = m + 1
+    for label in labels:
+        file.write(label.description + '\n')
+        print(label.description)
+    print('')
+    file.write('\n')
+file.close()
